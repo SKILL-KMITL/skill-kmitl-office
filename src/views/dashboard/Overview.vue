da023992f8c09426f95e6c6fb5da768033989337
vasin sermsampan <dev.vasin@gmail.com>
2021-03-19 19:08:36 +0700

    init auth

--- src/views/dashboard/Overview.vue
+++ src/views/dashboard/Overview.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     Overview
+    <v-btn @click="login()">Login Gmail</v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import { Component } from "vue-property-decorator";
 import Dashboard from "./Dashboard.vue";
+import OneGraphAuth from "onegraph-auth";
+
 @Component({})
-export default class Overview extends Dashboard {}
+export default class Overview extends Dashboard {
+  auth = new OneGraphAuth({
+    appId: "df4d06d4-c224-44d0-a660-63a685705e51"
+  });
+  mounted() {
+    this.auth.isLoggedIn("gmail").then((isLoggedIn: unknown) => {
+      if (isLoggedIn) {
+        console.log("Already logged in to Gmail");
+      } else {
+        console.log("Not logged in to Gmail.");
+      }
+    });
+  }
+
+  public login() {
+    this.auth.login("gmail");
+  }
+}
 </script>
