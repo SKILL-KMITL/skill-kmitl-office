53ae2a8e4893695c67824503ce3ad929c3b02759
vasin sermsampan <dev.vasin@gmail.com>
2021-03-20 20:41:06 +0700

    feat(store): init vuex store

---
+++ src/store/modules/user.ts
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
+import { User } from "@/@types/user";
+
+// https://github.com/championswimmer/vuex-module-decorators
+@Module({
+  name: "user",
+  namespaced: true,
+  stateFactory: true
+})
+export default class UserStore extends VuexModule {
+  user: User = new User();
+
+  @Mutation
+  setUser(data) {
+    this.user = data;
+  }
+
+  @Mutation
+  updateUser(data: Record<string, any>) {
+    this.user[data.key] = data.value;
+  }
+
+  @Mutation
+  resetUser() {
+    this.user = new User();
+  }
+
+  get userIsLoggedIn() {
+    return this.user.isLoggedIn;
+  }
+
+  get userData() {
+    return this.user.user;
+  }
+}
